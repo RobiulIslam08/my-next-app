ccf3b8d3fc8738c497d4468ea0a160f1caa54d8d
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-08-24 10:31:40 +0600

    Set-up Json Server for Next Level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomePage = async() => {
+  const res = await fetch('http://localhost:5000/products')
+  const product = await res.json()
+  console.log(product)
   return (
     <div>
       Home
