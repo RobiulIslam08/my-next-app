e6bac5659efb9ab21211ab5ce2efac01392949c5
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-08-24 10:37:54 +0600

    Explain SSG by Next-level Data Fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const HomePage = async() => {
-  const res = await fetch('http://localhost:5000/products')
+  const res = await fetch('http://localhost:5000/products', {
+    cache:"force-cache"
+  })
   const product = await res.json()
   console.log(product)
   return (
